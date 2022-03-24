308e4c537a6606005f8cd1406995738b801b4cf0
Tobin deKorne <tdekorne@gmail.com>
2022-03-23 21:52:29 -0700

    modifying base path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    basePath: '/educational-games',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
